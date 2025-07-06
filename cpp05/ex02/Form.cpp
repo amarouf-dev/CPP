88f6ef1c4fb045d685d7d802495b0841f6d696a7
Abdallah Marouf <amarouf@c4r1p1.1337.ma>
2025-07-07 00:17:25 +0100

    header files added

---
+++ cpp05/ex02/Form.cpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarouf <amarouf@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 23:32:33 by amarouf           #+#    #+#             */
+/*   Updated: 2025/07/05 15:24:16 by amarouf          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form(): _name("Form"), is_signed(false), gradeToSign(15), gradeToExecute(20) {}
+
+Form::Form(std::string Nname, const int NgradeToSign, const int NgradeToExecute): _name(Nname)
+	, gradeToSign(NgradeToSign), gradeToExecute(NgradeToExecute)
+{
+	is_signed = false;
+	if (this->gradeToSign < 1)
+	{
+        throw Form::GradeTooLowException();
+	}
+    if (this->gradeToSign > 150)
+    {
+	    throw Form::GradeTooHighException();
+	}
+	if (this->gradeToExecute < 1)
+    {
+	    throw Form::GradeTooLowException();
+	}
+    if (this->gradeToExecute > 150)
+    {
+	    throw Form::GradeTooHighException();
+	}
+}
+
+Form::Form(const Form& Nform): _name(Nform._name), gradeToSign(Nform.gradeToSign), gradeToExecute(Nform.gradeToExecute)
+{
+	this->is_signed = Nform.is_signed;
+}
+
+Form::~Form(){}
+
+Form& Form::operator=(const Form& Nform)
+{
+	this->is_signed = Nform.is_signed;
+	return *this;
+}
+
+// Exception classes
+
+const char* Form::GradeTooHighException::what()  const throw()
+{
+    return "Invalid Grade: Too High !\n";
+}
+
+const char* Form::GradeTooLowException::what()  const throw()
+{
+   return "Invalid Grade: Too Low !\n";
+}
+
+// Getters
+
+std::string Form::get_Name(void) const
+{
+	return (this->_name);
+}
+
+bool Form::getIs_signed(void) const
+{
+	return (this->is_signed);	
+}
+
+int Form::getGradeToSign(void) const
+{
+	return (this->gradeToSign);	
+}
+
+int Form::getGradeToExecute(void) const
+{
+	return (this->gradeToExecute);	
+}
+
+// Methods
+
+void Form::beSigned(Bureaucrat& br)
+{
+	if (br.getGrade() <= this->gradeToSign)
+		this->is_signed = true;
+	else
+		throw Form::GradeTooLowException();
+}
+
+std::ostream& operator<<(std::ostream& out, Form& f)
+{
+	if (f.getIs_signed())
+		out << f.get_Name() << ", Form's grade To Execute " <<
+		f.getGradeToExecute() <<
+		", Form's grade To Sign " <<
+		f.getGradeToSign() <<
+		", The Form is signed" << std::endl;
+	else
+		out << f.get_Name() <<
+		", Form's grade To Execute " <<
+		f.getGradeToExecute() <<
+		", Form's grade To Sighn " <<
+		f.getGradeToSign() <<
+		", The Form is not signed" << std::endl;
+	return (out);
+}
