24dd89feddfabf6698e015aa4c749800762c5e0e
Abdallah Marouf <amarouf@c3r4p10.1337.ma>
2025-08-13 16:17:41 +0100

    tp

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarouf <amarouf@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 16:06:45 by amarouf           #+#    #+#             */
+/*   Updated: 2025/08/13 16:13:38 by amarouf          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY__
+#define ARRAY__
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+	public:
+	Array()
+	{
+		   T[];
+	}
+	Array(unsigned int n)
+	{
+		   new T[n];
+	}
+};
+
+#endif
