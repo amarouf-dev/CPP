ed9e8432c1644d18107220054f8348952aebaa85
Hntprl <abdellahmarof@gmail.com>
2025-08-14 15:32:31 +0100

    07 need tests

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amarouf <amarouf@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abdellah <abdellah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:06:45 by amarouf           #+#    #+#             */
-/*   Updated: 2025/08/13 16:13:38 by amarouf          ###   ########.fr       */
+/*   Updated: 2025/08/14 15:31:32 by abdellah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,48 @@
 template <typename T>
 class Array
 {
+	private:
+
+	T			*data;
+	unsigned int size;
+
 	public:
-	Array()
+	Array(): data(), size(1) {}
+	Array(unsigned int n): data(new T[n]), size(n) {}
+	
+	Array& operator=(Array& arr)
 	{
-		   T[];
+		delete[] this.data;
+		
+		this->size = arr.size;
+		this->data = new T[arr.size];
+		
+		for (int i = 0; i < arr.size; i ++)
+		{
+			this->data[i] = arr.data[i];
+		}
 	}
-	Array(unsigned int n)
+	Array(Array& arr)
 	{
-		   new T[n];
+		this = &arr;
+	}
+	~Array()
+	{
+		delete[] data;
+	}
+
+	T& operator[](unsigned int index)
+	{
+		if (index >= size)
+		{
+			throw std::out_of_range;
+		}
+		else
+			return (data[index]);
+	}
+	int size()
+	{
+		return (size);
 	}
 };
 
