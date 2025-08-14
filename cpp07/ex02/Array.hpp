864aee5d9f645bc92de91b0a4cc35a5327637dc9
Hntprl <abdellahmarof@gmail.com>
2025-08-14 16:01:25 +0100

    cpp 07 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: abdellah <abdellah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:06:45 by amarouf           #+#    #+#             */
-/*   Updated: 2025/08/14 15:31:32 by abdellah         ###   ########.fr       */
+/*   Updated: 2025/08/14 15:59:25 by abdellah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,20 @@ class Array
 	private:
 
 	T			*data;
-	unsigned int size;
+	unsigned int _size;
 
 	public:
-	Array(): data(), size(1) {}
-	Array(unsigned int n): data(new T[n]), size(n) {}
+	Array(): data(), _size(1) {}
+	Array(unsigned int n): data(new T[n]), _size(n) {}
 	
 	Array& operator=(Array& arr)
 	{
 		delete[] this.data;
 		
-		this->size = arr.size;
-		this->data = new T[arr.size];
+		this->_size = arr._size;
+		this->data = new T[arr._size];
 		
-		for (int i = 0; i < arr.size; i ++)
+		for (int i = 0; i < arr._size; i ++)
 		{
 			this->data[i] = arr.data[i];
 		}
@@ -50,16 +50,16 @@ class Array
 
 	T& operator[](unsigned int index)
 	{
-		if (index >= size)
+		if (index >= _size)
 		{
-			throw std::out_of_range;
+			throw std::out_of_range();
 		}
 		else
 			return (data[index]);
 	}
-	int size()
+	unsigned int size()
 	{
-		return (size);
+		return (_size);
 	}
 };
 
